765fe0672d91088ee10fc4dbc8de1675a1aa5e41
grounzero <michael.grounds@sky.uk>
2026-03-06 14:40:37 +0000

    remove using

--- cs/src/AlpacaFleece.Worker/GlobalUsings.cs
+++ cs/src/AlpacaFleece.Worker/GlobalUsings.cs
@@ -2,6 +2,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Globalization;
 global using System.Linq;
+global using System.Text.Json;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using MailKit.Net.Smtp;
